4d10d9f6e4eba8114c57a88ae528a3c5f5d57549
Paresh Shrivastava <paresh.shrivastava.jiit@gmail.com>
2014-11-14 00:50:56 -0800

    removing value

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var pdata =require('./routes/index1');//workout =pdata
+var pdata =require('./routes/index1');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/workout_tracker');
