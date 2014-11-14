8fcb3ce706138060fa10c6ac1fe550915cabf642
Paresh Shrivastava <paresh.shrivastava.jiit@gmail.com>
2014-11-14 01:14:30 -0800

    commit6

--- app.js
+++ app.js
@@ -33,42 +33,7 @@ app.get('/index1');
 app.get('/index1/:id');
 /*
 
-app.post('/index/create',function(req,res)
-{
- var workout_name = req.body.workout_name;
- var description = req.body.workout_description;
- 
-  Workout.findOne({ name: { $regex: new RegExp(workout_name, "i") } }, function(err, doc) {  // Using RegEx - search is case insensitive
-    if(!err && !doc) {
-      
-      var newWorkout = new Workout(); 
 
-      newWorkout.name = workout_name; 
-      newWorkout.description = description; 
-      
-      newWorkout.save(function(err) {
-
-        if(!err) {
-          res.json(201, {message: "Workout created with name: " + newWorkout.name });    
-        } else {
-          res.json(500, {message: "Could not create workout. Error: " + err});
-        }
-
-      });
-
-    } else if(!err) {
-      
-      // User is trying to create a workout with a name that already exists. 
-      res.json(403, {message: "Workout with that name already exists, please update instead of create or create a new workout with a different name."}); 
-
-    } else {
-      res.json(500, { message: err});
-    } 
-  });
-});
-
-
-*/
 
 
 //app.get('/workout',workout.index)
